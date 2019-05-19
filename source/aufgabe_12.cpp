1cbba3a9f171d9ca52aa2bb8eb357740f2c05720
Irene Lopez Garcia <lopezg.irene@gmail.com>
2019-05-19 09:29:10 +0200

    Aufgabe 12 - Sum two vectors' elements

---
+++ source/aufgabe_12.cpp
@@ -0,0 +1,30 @@
+#define CATCH_CONFIG_RUNNER
+#include <catch.hpp>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <iostream>
+
+
+bool is_10(unsigned int num) {
+  return num == 10;
+}
+
+TEST_CASE (" all sums are 10 ", "[ sum ]")
+{
+
+  std::vector<int> v_1{1 ,2 ,3 ,4 ,5 ,6 ,7 ,8 ,9};
+  std::vector<int> v_2{9 ,8 ,7 ,6 ,5 ,4 ,3 ,2 ,1};
+  std::vector<int> v_3(9);
+
+  for (int i = 0; i < v_3.size(); ++i) {
+    v_3[i] = v_1.at(i) + v_2.at(i);
+  }
+
+  REQUIRE(std::all_of(v_3.begin(), v_3.end(), is_10));
+}
+
+int main (int argc , char * argv [])
+{
+  return Catch::Session (). run(argc , argv );
+}
